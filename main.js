762537a6adb21152d5f60578a782a64d6348ca5a
blnc97 <108918106+blnc97@users.noreply.github.com>
2022-09-26 20:53:00 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "31/08/2022", "Genesis Block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let lCoin = new Blockchain();
+lCoin.addBlock(new Block(1, "31/08/2022", { amount: 1 }));
+lCoin.addBlock(new Block(2, "1/09/2022", { amount: 10 }));
+
+console.log('Is blockchain valid? ' + lCoin.isChainValid());
+
+/*     tamper
+
+lCoin.chain[1].data = { amount: 99999999 };
+lCoin.chain[1].hash = lCoin.chain[1].calculateHash();
+
+console.log('Is blockchain valid? ' + lCoin.isChainValid());
+*/
+
+
+// console.log(JSON.stringify(lCoin, null, 4));
